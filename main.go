d9c92c9a20c423d0479b099ed72be79dee1c9e5f
anasMuf <anas.muhammadakbar@gmail.com>
2025-06-17 11:31:41 +0700

    fix port

--- main.go
+++ main.go
@@ -6,6 +6,7 @@ import (
 	"employee-management-app/repositories"
 	"log"
 	"net/http"
+	"os"
 
 	"github.com/julienschmidt/httprouter"
 )
@@ -26,14 +27,14 @@ func main() {
 	router.PUT("/employees/:id", empHandler.UpdateEmployee)
 	router.DELETE("/employees/:id", empHandler.DeleteEmployee)
 
-	log.Println("Server running at http://localhost:8080")
-	log.Fatal(http.ListenAndServe(":8080", router))
-	// port := os.Getenv("PORT")
-	// if port == "" {
-	// 	port = "8080"
-	// }
-	// err := http.ListenAndServe(":"+port, router)
-	// if err != nil {
-	// 	log.Fatal("server gagal dijalankan", err)
-	// }
+	// log.Println("Server running at http://localhost:8080")
+	// log.Fatal(http.ListenAndServe(":8080", router))
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "8080"
+	}
+	err := http.ListenAndServe(":"+port, router)
+	if err != nil {
+		log.Fatal("server gagal dijalankan", err)
+	}
 }
